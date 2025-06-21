ef2ab2fb1ea44667356497f6e7d696094836a220
cketiel <cketielbr@gmail.com>
2025-06-20 22:32:01 -0600

    Add UnscheduledTripDto

---
+++ Meditrans.Shared/DTOs/UnscheduledTripDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Meditrans.Shared.DTOs
+{
+    public class UnscheduledTripDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string CustomerName { get; set; }
+        public TimeSpan? FromTime { get; set; }
+        public TimeSpan? ToTime { get; set; }
+        public string PickupAddress { get; set; }
+        public string DropoffAddress { get; set; }
+        public string SpaceType { get; set; }
+        public string FundingSource { get; set; }
+    }
+}
