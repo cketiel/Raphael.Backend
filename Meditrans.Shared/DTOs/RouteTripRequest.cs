0ac3fb7c11530ca754a66448242c8fa84a36f2bd
cketiel <cketielbr@gmail.com>
2025-06-20 22:33:33 -0600

    Add RouteTripRequest

---
+++ Meditrans.Shared/DTOs/RouteTripRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meditrans.Shared.DTOs
+{
+    public class RouteTripRequest
+    {
+        [Required]
+        public int VehicleRouteId { get; set; }
+        [Required]
+        public List<int> TripIds { get; set; }
+    }
+}
