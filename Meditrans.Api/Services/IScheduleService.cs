ac92516e11f514064775dd05533b0c4931ad6eaa
cketiel <cketielbr@gmail.com>
2025-06-20 22:37:44 -0600

    Add IScheduleService

---
+++ Meditrans.Api/Services/IScheduleService.cs
@@ -0,0 +1,12 @@
+﻿using Meditrans.Shared.DTOs;
+
+namespace Meditrans.Api.Services
+{
+    public interface IScheduleService
+    {
+        Task<IEnumerable<ScheduleDto>> GetSchedulesByRouteAndDateAsync(int vehicleRouteId, DateTime date);
+        Task<IEnumerable<UnscheduledTripDto>> GetUnscheduledTripsByDateAsync(DateTime date);
+        Task RouteTripsAsync(RouteTripRequest request);
+        Task CancelRouteForTripAsync(int scheduleId);
+    }
+}
