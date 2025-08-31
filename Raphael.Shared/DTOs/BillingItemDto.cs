df637cc5b38ac0ed350b0ca39bf0e5d66b4ab98e
cketiel <cketielbr@gmail.com>
2025-08-31 16:46:47 -0600

    Add BillingItemDto

---
+++ Raphael.Shared/DTOs/BillingItemDto.cs
@@ -0,0 +1,38 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Raphael.Shared.DbContexts;
+using Raphael.Shared.Dtos;
+using Raphael.Shared.Entities;
+
+namespace Raphael.Shared.Dtos
+{
+    public class BillingItemDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        [Required]
+        public int UnitId { get; set; }
+
+        public bool IsCopay { get; set; }
+
+        [StringLength(50)]
+        public string? ARAccount { get; set; }
+
+        [StringLength(50)]
+        public string? ARSubAccount { get; set; }
+
+        [StringLength(50)]
+        public string? ARCompany { get; set; }
+
+        [StringLength(50)]
+        public string? APAccount { get; set; }
+
+        [StringLength(50)]
+        public string? APSubAccount { get; set; }
+
+        [StringLength(50)]
+        public string? APCompany { get; set; }
+    }
+}
