973448651f04b5399552d9b1072a7325d387f96a
cketiel <cketielbr@gmail.com>
2025-09-16 03:15:54 -0600

    Add SignatureUploadDto

---
+++ Raphael.Shared/DTOs/SignatureUploadDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Raphael.Shared.DTOs
+{
+    public class SignatureUploadDto
+    {
+        // The signature will be sent as a Base64 string, which is the standard for JSON/HTTP
+        public string SignatureBase64 { get; set; }
+    }
+}
