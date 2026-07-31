fce4af4c8f977f401a925bba24ab0d5eb9d547ef
cketiel <cketielbr@gmail.com>
2026-07-30 18:11:07 -0600

    feat(shared): add Enumeration base class - Added reusable Enumeration base class to Raphael.Shared - Introduced support for Id, Code and Name properties - Implemented lookup methods for retrieving enumeration instances - Established a shared foundation for strongly typed domain enumerations across the Raphael ecosystem

---
+++ Raphael.Shared/Domain/Common/Enumeration.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+
+namespace Raphael.Shared.Domain.Common;
+
+public abstract class Enumeration : IComparable
+{
+    public int Id { get; }
+
+    public string Code { get; }
+
+    public string Name { get; }
+
+    protected Enumeration(int id, string code, string name)
+    {
+        Id = id;
+        Code = code;
+        Name = name;
+    }
+
+    public override string ToString()
+        => Name;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Enumeration other)
+            return false;
+
+        return GetType() == other.GetType()
+            && Id == other.Id;
+    }
+
+    public override int GetHashCode()
+        => HashCode.Combine(GetType(), Id);
+
+    public int CompareTo(object? other)
+    {
+        if (other is not Enumeration enumeration)
+            throw new ArgumentException("Object is not an Enumeration.");
+
+        return Id.CompareTo(enumeration.Id);
+    }
+
+    public static IEnumerable<T> GetAll<T>()
+        where T : Enumeration
+    {
+        return typeof(T)
+            .GetFields(BindingFlags.Public |
+                       BindingFlags.Static |
+                       BindingFlags.DeclaredOnly)
+            .Select(f => f.GetValue(null))
+            .Cast<T>();
+    }
+
+    public static T FromId<T>(int id)
+        where T : Enumeration
+    {
+        return GetAll<T>().Single(x => x.Id == id);
+    }
+
+    public static T FromCode<T>(string code)
+        where T : Enumeration
+    {
+        return GetAll<T>()
+            .Single(x => x.Code.Equals(code, StringComparison.OrdinalIgnoreCase));
+    }
+}
